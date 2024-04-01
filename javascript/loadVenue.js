477448a94471a9723def44cd58518b3cbea63cad
Yuval Ramati <yuvalra@gmail.com>
2024-04-01 15:51:45 +0300

    Whisper changes

--- javascript/loadVenue.js
+++ javascript/loadVenue.js
@@ -9,10 +9,10 @@ let panorama, viewer, container;
 
 var pusher = null;
 const myHeaders = new Headers();
-        myHeaders.append("x-api-key", "g8Vu69Ip2RJAEuFVgAxoN9QKq7DmViQtW4jfo5CwIENvU7Fk4URYMckq94Mq");
+myHeaders.append("x-api-key", "g8Vu69Ip2RJAEuFVgAxoN9QKq7DmViQtW4jfo5CwIENvU7Fk4URYMckq94Mq");
   
   // Subscribe to the channel you want to receive events from
-  var channel = null; 
+var channel = null; 
  window.loadVenueEvent = async function loadVenueEvent(eventData) {
   console.log(eventData);
   if(eventData.status == 'complete'){
@@ -20,18 +20,7 @@ const myHeaders = new Headers();
      imageSettings.image = eventData.file_url;
      displayImage(imageSettings)
   }
-  //var requestOptions = {
-  //  method: "GET",
-  //  headers: myHeaders,
-  //  redirect: "follow"
-  //  };
-
-   //const response2 = await fetch(`https://backend.blockadelabs.com/api/v1/imagine/requests/${data.id}`, requestOptions);
-   //const response2 = await fetch("https://backend.blockadelabs.com/api/v1/imagine/requests/10553724", requestOptions);
-   //const data2 = await response2.json();
-   //imagine_file_url = data2['request']['file_url'];
 }
-
 
 window.startWhisper = async function startWhisper() {
     container = document.querySelector('#container');
@@ -53,15 +42,17 @@ window.startWhisper = async function startWhisper() {
     viewer = new PANOLENS.Viewer({ container: container });
     panorama.add(infospot);
     viewer.add(panorama);
+    
   
-    var ef = 0, ctrl = 0;
+    var ef = 1, ctrl = 0;
     if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(navigator.userAgent)) {
         ef = 2;
         ctrl = 1;
     }
     viewer.enableControl(ctrl);
-    viewer.enableEffect(1);
+    viewer.enableEffect(ef);
     viewer.setCameraFov(90);
+    
   }
   
 
@@ -70,7 +61,7 @@ window.displayImage = async function displayImage(imageSettings) {
 
   var audio = new Audio(imageSettings.audio || 'assets/imagine_l1.mp3');
   audio.loop = true;
-  audio.play();
+  //audio.play();
 
   let infospot = new PANOLENS.Infospot(300, 'assets/notes4.png', true);
 
@@ -83,10 +74,17 @@ window.displayImage = async function displayImage(imageSettings) {
   panorama.addEventListener('enter-fade-start', () => {
       viewer.tweenControlCenter(new THREE.Vector3(imagePosition[0],imagePosition[1],imagePosition[2]), 0)
   })
-  viewer = new PANOLENS.Viewer({ container: container });
-  viewer.add(panorama);
 
-  var ef = 0, ctrl = 0;
+  if (viewer == null) {
+    viewer = new PANOLENS.Viewer({ container: container });
+    viewer.add(panorama);
+  }
+  else {
+    viewer.dispose();
+    viewer.add(panorama);
+  }
+
+  var ef = 1, ctrl = 0;
   if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(navigator.userAgent)) {
       ef = 2;
       ctrl = 1;
@@ -95,6 +93,42 @@ window.displayImage = async function displayImage(imageSettings) {
   viewer.enableEffect(ef);
   viewer.setCameraFov(90);
 }
+
+window.displayGeneratedImage = async function displayGeneratedImage(imageSettings) {
+    //const imageSettings = imagesSettings[imageName];
+  
+    var audio = new Audio(imageSettings.audio || 'assets/imagine_l1.mp3');
+    audio.loop = true;
+    audio.play();
+  
+    let infospot = new PANOLENS.Infospot(300, 'assets/notes4.png', true);
+  
+    infospot.position.set(0, -1000, -1000);
+    infospot.addHoverElement(document.getElementById('cheat_note'), 200);
+    panorama = new PANOLENS.ImagePanorama(imageSettings.image || 'assets/' + imageName, 50);
+    panorama.add(infospot);
+  
+    const imagePosition = imageSettings.position;
+    panorama.addEventListener('enter-fade-start', () => {
+        viewer.tweenControlCenter(new THREE.Vector3(imagePosition[0],imagePosition[1],imagePosition[2]), 0)
+    })
+    if (viewer == null) {
+        viewer = new PANOLENS.Viewer({ container: container });
+        viewer.add(panorama);
+      }
+      else {
+        viewer.setPanorama(panorama);
+      }
+  
+    var ef = 1, ctrl = 0;
+    if (/Mobile|Android|iP(hone|od)|IEMobile|BlackBerry|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(navigator.userAgent)) {
+        ef = 2;
+        ctrl = 1;
+    }
+    viewer.enableControl(ctrl);
+    viewer.enableEffect(ef);
+    viewer.setCameraFov(90);
+  }
 
 window.loadVenue = async function loadVenue() {
     noSleep.enable();
@@ -141,3 +175,34 @@ window.loadVenue = async function loadVenue() {
       displayImage(imagesSettings[imageName]);
      }
 }
+window.loadWhisperVenue = async function loadWhisperVenue(imagePrompt) {
+    noSleep.enable();
+    document.getElementById("btn").style.display = "none";
+
+    container = document.querySelector('#container');
+          
+    //trying to generate an image from the SkyBox
+    
+    let requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        redirect: "follow"
+    };
+
+    const response = await fetch(`https://backend.blockadelabs.com/api/v1/skybox?prompt=${imagePrompt}`, requestOptions);
+    const data = await response.json();
+    console.log(data);
+
+    Pusher.logToConsole = true;
+    pusher = new Pusher('a6a7b7662238ce4494d5', {
+    appId: "1555452",
+    key: "a6a7b7662238ce4494d5",
+    secret: "5b69dc3f0bb7800ed8d0",
+    cluster: "mt1"
+    });
+    
+    // Subscribe to the channel you want to receive events from
+    channel = pusher.subscribe(data.pusher_channel);
+    channel.bind(data.pusher_event, loadVenueEvent);      
+}
+
