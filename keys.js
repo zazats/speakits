1fd04f75f36e7e7398856b44884180156e18e4f5
yr1971 <164202490+yr1971@users.noreply.github.com>
2024-04-10 09:13:25 +0200

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,2 +1,3 @@
-window.CHATGPT = "ZAZAsk-XFDNXbmrhDabGoMFylK7T3BlbkFJjPrDzfSeRtfnTyTQ8drw";
+window.CHATGPT1 = "sk-K8XNRar30DASFvEXX5EP";
+window.CHATGPT2 = "T3BlbkFJi1FM9thmqoO8LsYBQPKA";
 window.IMAGINE =  "g8Vu69Ip2RJAEuFVgAxoN9QKq7DmViQtW4jfo5CwIENvU7Fk4URYMckq94Mq";
